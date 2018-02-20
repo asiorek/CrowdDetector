4652ee13aaedcc09c9ee5745994ac9e4f7701904
Jasmine Karlsson <j.a.c.karlsson@student.vu.nl>
2018-02-20 16:36:04 +0100

    Added button and image

--- app/src/main/java/nl/uva/mobile/crowddetector/MainActivity.java
+++ app/src/main/java/nl/uva/mobile/crowddetector/MainActivity.java
@@ -2,12 +2,26 @@ package nl.uva.mobile.crowddetector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getCanonicalName();
+    Button mybutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mybutton = findViewById(R.id.button);
+
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Log.d(TAG, "onClick: Button clicked");
+            }
+        });
     }
+
 }
