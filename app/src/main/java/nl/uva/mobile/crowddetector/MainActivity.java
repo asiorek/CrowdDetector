c8afca6b96946a6b59a01868183a197a9cc87fda
Jasmine Karlsson <j.a.c.karlsson@student.vu.nl>
2018-03-13 14:30:42 +0100

    Added dropdown menus for start-page

--- app/src/main/java/nl/uva/mobile/crowddetector/MainActivity.java
+++ app/src/main/java/nl/uva/mobile/crowddetector/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 //TODO: Create buttons for getting information for each floor
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mybutton = findViewById(R.id.button);
+        Spinner ddcity = findViewById(R.id.city);
+        Spinner dduni = findViewById(R.id.university);
+        Spinner ddfloor = findViewById(R.id.floor);
+
+
 
         final Intent intent = new Intent(this, HeatMapWebViewActivity.class);
 
@@ -28,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: Button clicked");
             }
         });
+
+
+
+        //get the spinner from the xml.
+
+        //create a list of items for the spinner.
+        String[] cities = new String[]{"Amstedam", "Rotterdam", "Utrecht"};
+        String[] unis = new String[]{"UvA", "VU", "HvA"};
+        String[] floors = new String[]{"Ground floor", "1st floor", "2nd floor"};
+
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cities);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, unis);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, floors);
+
+        //set the spinners adapter to the previously created one.
+        ddcity.setAdapter(adapter);
+        dduni.setAdapter(adapter2);
+        ddfloor.setAdapter(adapter3);
     }
 
     @Override
