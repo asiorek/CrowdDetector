dc43836532e9d72339b40b405254e6d71a02ca1e
Jasmine Karlsson <j.a.c.karlsson@student.vu.nl>
2018-03-20 14:35:03 +0100

    Change data spots

--- app/src/main/java/nl/uva/mobile/crowddetector/rest/ServiceGenerator.java
+++ app/src/main/java/nl/uva/mobile/crowddetector/rest/ServiceGenerator.java
@@ -2,7 +2,6 @@ package nl.uva.mobile.crowddetector.rest;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 /**
