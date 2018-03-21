9d739557ae6e882b0f61e3313c2c9fab5ca3e344
Jasmine Karlsson <j.a.c.karlsson@student.vu.nl>
2018-03-21 15:02:24 +0100

    Random data

--- app/src/main/assets/data.js
+++ app/src/main/assets/data.js
@@ -1,2 +1,24 @@
 // taken from https://github.com/pa7/heatmap.js (MIT-licensed)
-var data = [[38,20,2],[1900,900,1],[1780,830,1],[1720,790,1],[1340,890,1],[1900,450,1],[1900,300,1][1700,900,1]];
+var data = [[38,20,2],[1600,20,2]];
+
+
+for (var i = 0; i <200; i++)
+{
+    x = 700 + 450*Math.random();
+    y = 900*Math.random();
+    data.push([x,y]);
+}
+
+for (var i = 0; i <80; i++)
+{
+    x = 600+1000*Math.random();
+    y = 250*Math.random();
+    data.push([x,y]);
+}
+
+for (var i = 0; i <80; i++)
+{
+    x = 600+1000*Math.random();
+    y = 250*Math.random();
+    data.push([x,y]);
+}
